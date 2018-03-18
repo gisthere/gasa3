2046534efadf330f13843dedd0201e32ed5f43ab
Fanis Kalimullin <fmkalimullin@gmail.com>
2018-03-18 22:19:31 +0300

    initial with tests

---
+++ src/test/java/ShapeClassifierTest.java
@@ -0,0 +1,57 @@
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
+public class ShapeClassifierTest {
+
+    @Test
+    @FileParameters("src/test/1wise.csv")
+    public void test1wise(String shape, String size, String even, String p1, String p2, String p3, String p4, String res) {
+        String str = buildString(shape, size, even, p1, p2, p3, p4, res);
+        assertEquals(res, new ShapeClassifier().evaluateGuess(str));
+    }
+
+    @Test
+    @FileParameters("src/test/2wise.csv")
+    public void test2wise(String shape, String size, String even, String p1, String p2, String p3, String p4, String res) {
+        String str = buildString(shape, size, even, p1, p2, p3, p4, res);
+        assertEquals(res, new ShapeClassifier().evaluateGuess(str));
+    }
+
+    @Test
+    @FileParameters("src/test/3wise.csv")
+    public void test3wise(String shape, String size, String even, String p1, String p2, String p3, String p4, String res) {
+        String str = buildString(shape, size, even, p1, p2, p3, p4, res);
+        assertEquals(res, new ShapeClassifier().evaluateGuess(str));
+    }
+
+    @Test
+    @FileParameters("src/test/manual.csv")
+    public void testManual(String shape, String size, String even, String p1, String p2, String p3, String p4, String res) {
+        String str = buildString(shape, size, even, p1, p2, p3, p4, res);
+        assertEquals(res, new ShapeClassifier().evaluateGuess(str));
+    }
+
+    private String buildString(String shape, String size, String even, String p1, String p2, String p3, String p4, String res) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(shape).append(',');
+        sb.append(size).append(',');
+        sb.append(even).append(',');
+        sb.append(p1).append(',');
+        if (!p2.equals("NA")) {
+            sb.append(p2).append(',');
+        }
+        if (!p3.equals("NA")) {
+            sb.append(p3).append(',');
+        }
+        if (!p4.equals("NA")) {
+            sb.append(p4).append(',');
+        }
+
+        return sb.toString();
+    }
+}
