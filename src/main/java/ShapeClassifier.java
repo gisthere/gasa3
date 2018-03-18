c4c688fc0e8631311d19bb0ea4c7645677b74a02
Fanis Kalimullin <fmkalimullin@gmail.com>
2018-03-19 00:13:18 +0300

    fix wrong ellipse guess bug

--- src/main/java/ShapeClassifier.java
+++ src/main/java/ShapeClassifier.java
@@ -169,16 +169,11 @@ public class ShapeClassifier {
 
     // classify an two sides
     private String classify2Parameters(int a, int b) {
-        if (a == b) {
-            return twoParamGuesses[0];
-        } else if (a == 0) {
-            if (b > 0) {
+        if (a >= 1 && b >= 1) {
+            if (a == b)
+                return twoParamGuesses[0];
+            else
                 return twoParamGuesses[1];
-            }
-        } else if (a > 1) {
-            if (b != 0) {
-                return twoParamGuesses[1];
-            }
         }
         return "";
     }
