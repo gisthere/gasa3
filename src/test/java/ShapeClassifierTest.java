649c223841ed07b7ab018e24a31de904a0a2791c
Fanis <fmkalimullin@gmail.com>
2018-03-21 21:01:44 +0300

    white box

--- src/test/java/ShapeClassifierTest.java
+++ src/test/java/ShapeClassifierTest.java
@@ -36,6 +36,13 @@ public class ShapeClassifierTest {
         assertEquals(res, new ShapeClassifier().evaluateGuess(str));
     }
 
+    @Test
+    @FileParameters("src/test/whitebox.csv")
+    public void testWhiteBox(String shape, String size, String even, String p1, String p2, String p3, String p4, String p5, String res) {
+        String str = buildString(shape, size, even, p1, p2, p3, p4, p5, res);
+        assertEquals(res, new ShapeClassifier().evaluateGuess(str));
+    }
+
     private String buildString(String shape, String size, String even, String p1, String p2, String p3, String p4, String res) {
         StringBuilder sb = new StringBuilder();
         sb.append(shape).append(',');
@@ -49,6 +56,28 @@ public class ShapeClassifierTest {
             sb.append(p3).append(',');
         }
         if (!p4.equals("NA")) {
+            sb.append(p4).append(',');
+        }
+
+        return sb.toString();
+    }
+
+    private String buildString(String shape, String size, String even, String p1, String p2, String p3, String p4, String p5, String res) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(shape).append(',');
+        sb.append(size).append(',');
+        sb.append(even).append(',');
+        sb.append(p1).append(',');
+        if (!p2.equals("NA")) {
+            sb.append(p2).append(',');
+        }
+        if (!p3.equals("NA")) {
+            sb.append(p3).append(',');
+        }
+        if (!p4.equals("NA")) {
+            sb.append(p4).append(',');
+        }
+        if (!p5.equals("NA")) {
             sb.append(p4).append(',');
         }
 
